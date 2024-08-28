1d4c1ebf524975b478e7dacf0b0634265d93c612
NeeratiChandu <129150574+NeeratiChandu@users.noreply.github.com>
2024-08-28 19:39:37 +0530

    Create AppContainer.js

---
+++ AppContainer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import CategoryFilter from './CategoryFilter';
+import App from './App';
+
+function AppContainer() {
+  return (
+    <div>
+      <App />
+      <CategoryFilter />
+    </div>
+  );
+}
+
+export default AppContainer;
