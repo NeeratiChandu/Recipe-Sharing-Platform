1c2d033d3d725be5f61c72cbf4c4dd5a1f872c62
NeeratiChandu <129150574+NeeratiChandu@users.noreply.github.com>
2024-08-31 19:33:06 +0530

    Create Register.js

---
+++ Register.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+
+function Register({ onRegister }) {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onRegister(username, password);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Username:
+        <input type="text" value={username} onChange={handleUsernameChange} />
+      </label>
+      <br />
+      <label>
+        Password:
+        <input type="password" value={password} onChange={handlePasswordChange} />
+      </label>
+      <br />
+      <button type="submit">Register</button>
+    </form>
+  );
+}
+
+export default Register;
